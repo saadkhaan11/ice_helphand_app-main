0aa51fd3fce47a8805bac3c3f64edb515c7e7983
Amir-Yaseen <63316291+Amir-Yaseen@users.noreply.github.com>
2023-04-25 15:52:38 +0500

    Update map_screen.dart

--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,12 +1,8 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../../models/markerdata.dart';
-import 'dart:math' show cos, sqrt, asin;
 // import 'package:geoflutterfire/geoflutterfire.dart';
 
 class MapScreen extends StatefulWidget {
@@ -19,71 +15,10 @@ class MapScreen extends StatefulWidget {
 
 class MapScreenState extends State<MapScreen> {
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-  User? user = FirebaseAuth.instance.currentUser;
-
   // final geo = Geoflutterfire();
   //geolocator
-  // String? _currentAddress;
-  static Position? _currentPosition;
-  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
-  bool isLoading = true;
-  List<MarkerData> markerDataList = [];
-  Set<Marker> markers = {};
-  static GoogleMapController? _googleMapController;
-  Set<Marker> markersInRadius = {};
-  void initMarker(data, id) {
-    print('initmarker');
-    MarkerId markerId = MarkerId(id);
-    final Marker marker = Marker(
-        markerId: markerId,
-        position:
-            LatLng(data['location'].latitude, data['location'].longitude));
-    // setState(() {
-    markers.add(marker);
-    getMarkersInRadius(LatLng(37.4219999, -122.0840575), 500);
-    // });
-    print(markers.length);
-    // markers[markerId] = marker;
-  }
-
-  double distanceBetween(LatLng point1, LatLng point2) {
-    const p = 0.017453292519943295;
-    const c = cos;
-    final a = 0.5 -
-        c((point2.latitude - point1.latitude) * p) / 2 +
-        c(point1.latitude * p) *
-            c(point2.latitude * p) *
-            (1 - c((point2.longitude - point1.longitude) * p)) /
-            2;
-    return 12742 * asin(sqrt(a));
-  }
-
-  void getMarkersInRadius(LatLng center, double radius) {
-    markers.forEach((marker) {
-      if (distanceBetween(center, marker.position) <= radius) {
-        setState(() {
-          markersInRadius.add(marker);
-        });
-      }
-    });
-    // return markersInRadius;
-  }
-
-  void locationData() {
-    Future<QuerySnapshot<Map<String, dynamic>>> querySnapshot =
-        firebaseFirestore.collection("usersLocation").get();
-    querySnapshot.then((snapshot) {
-      if (snapshot.docs.isNotEmpty) {
-        for (int i = 0; i < snapshot.docs.length; i++) {
-          initMarker(snapshot.docs[i].data(), snapshot.docs[i].id);
-          getMarkersInRadius(
-              LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
-              1.1);
-          print("Doclist${snapshot.docs[i].data()['locaion']}");
-        }
-      }
-    });
-  }
+  String? _currentAddress;
+  Position? _currentPosition;
 
   Future<bool> _handleLocationPermission() async {
     bool serviceEnabled;
@@ -116,49 +51,24 @@ class MapScreenState extends State<MapScreen> {
 
   Future<void> _getCurrentPosition() async {
     final hasPermission = await _handleLocationPermission();
-    // print('object');
+    print('object');
 
     if (!hasPermission) return;
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-        isLoading = false;
-        print("Loading :$isLoading");
-      });
-      updateLocation(position.latitude, position.longitude);
-      // print(position.latitude);
+      setState(() => _currentPosition = position);
+      print(position.latitude);
       // _getAddressFromLatLng(_currentPosition!);
     }).catchError((e) {
       debugPrint(e);
     });
   }
-
-  void updateLocation(double latitude, double longitude) {
-    GeoPoint location = GeoPoint(latitude, longitude);
-    firebaseFirestore.collection("usersLocation").doc(user!.uid).set({
-      "location": location,
-    });
-  }
-
-  void addCustomIcon() {
-    BitmapDescriptor.fromAssetImage(
-            const ImageConfiguration(), "assets/images/mylocation.png")
-        .then(
-      (icon) {
-        setState(() {
-          markerIcon = icon;
-        });
-      },
-    );
-  }
-
   //
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
@@ -173,76 +83,28 @@ class MapScreenState extends State<MapScreen> {
     distanceFilter: 100,
   );
 
-  @override
+      @override
   void initState() {
-    addCustomIcon();
-    _getCurrentPosition();
-    // StreamSubscription<Position> positionStream =
-    //     Geolocator.getPositionStream(locationSettings: locationSettings)
-    //         .listen((Position? position) {
-    //   _getCurrentPosition();
-    // });
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+            _getCurrentPosition();
+    });
     // TODO: implement initState
     super.initState();
+            
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection("usersLocation")
-                  .snapshots(),
-              builder: (context, snapshot) {
-                print(snapshot);
-                if (snapshot.hasData) {
-//Extract the location from document
-                  GeoPoint location = snapshot.data!.docs.first.get("location");
-// Check if location is valid
-                  if (location == null) {
-                    return Text("There was no location data");
-                  }
-                  // Remove any existing markers
-                  // markers.clear();
-                  // final latLng = LatLng(location.latitude, location.longitude);
-// Add new marker with markerId.
-                  // markers.add(
-                  //     Marker(markerId: MarkerId("location"), position: latLng));
-// If google map is already created then update camera position with animation
-                  // _googleMapController
-                  //     ?.animateCamera(CameraUpdate.newCameraPosition(
-                  //   CameraPosition(
-                  //     target: latLng,
-                  //     zoom: 15,
-                  //   ),
-                  // ));
-                  return GoogleMap(
-                    mapType: MapType.normal,
-                    initialCameraPosition: _kGooglePlex,
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller.complete(controller);
-                    },
-                    markers: markersInRadius,
-                    circles: {
-                      Circle(
-                          circleId: CircleId('1'),
-                          center: LatLng(
-                            _currentPosition!.latitude,
-                            _currentPosition!.longitude,
-                          ),
-                          radius: 500,
-                          fillColor: Color.fromARGB(59, 33, 149, 243),
-                          strokeWidth: 2,
-                          strokeColor: Color.fromARGB(202, 255, 255, 255))
-                    },
-                  );
-                }
-                return CircularProgressIndicator();
-              }),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
         label: const Text('To the lake!'),
@@ -254,8 +116,9 @@ class MapScreenState extends State<MapScreen> {
   void _goToTheLake() async {
     // final GoogleMapController controller = await _controller.future;
     // controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-    locationData();
-    // print(_currentPosition?.latitude);
-    // print(_currentPosition?.longitude);
+    _getCurrentPosition();
+    print(_currentPosition?.latitude);
+     print(_currentPosition?.longitude);
+
   }
 }
